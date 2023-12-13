d9bf6be1156b954a7b8fc30f2866f85920496685
Bohdan Soldatenko <144493957+trumom4r@users.noreply.github.com>
2023-12-13 21:18:31 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -15,3 +15,44 @@ function calculate1() {
 
     document.getElementById('result1').innerHTML = "Результат: "+workforceRate.toFixed(0);
 }
+
+function submitQuiz() {
+    var answersQ1 = document.querySelector('input[name="q1"]:checked');
+    var answersQ2 = document.querySelector('input[name="q2"]:checked');
+    var answersQ3 = document.querySelector('input[name="q3"]:checked');
+
+
+if (!answersQ1 || !answersQ2 || !answersQ3) {
+    alert("Будь ласка, виберіть відповіді на всі питання.");
+    return;
+}
+
+var answerValueQ1 = answersQ1.value;
+var answerValueQ2 = answersQ2.value;
+var answerValueQ3 = answersQ3.value;
+
+var correctAnswers = {
+    q1: 'c',
+    q2: 'a',
+    q3: 'a'
+}
+
+if (answerValueQ1 === correctAnswers.q1) {
+    document.getElementById('answer').innerHTML = 'Відповідь: вірна';
+}
+else {
+    document.getElementById('answer').innerHTML = 'Відповідь: невірна';
+}
+if (answerValueQ1 === correctAnswers.q2) {
+    document.getElementById('answer2').innerHTML = 'Відповідь: вірна';
+}
+else {
+    document.getElementById('answer2').innerHTML = 'Відповідь: невірна';
+}
+if (answerValueQ1 === correctAnswers.q3) {
+    document.getElementById('answer3').innerHTML = 'Відповідь: вірна';
+}
+else {
+    document.getElementById('answer3').innerHTML = 'Відповідь: невірна';
+}
+}
